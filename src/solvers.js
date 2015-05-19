2e79fd6014dbef7780a2177df6baa46a38205752
smkhalsa <smkhalsa@gmail.com>
2015-05-19 15:09:53 -0700

    Completed countNRooksSolutions and countNQueensSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -32,10 +32,6 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  // var solutionCount = undefined; //fixme
-
-  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  // return solutionCount;
 
   // create new board of size n
   var board = new Board({n:n});
@@ -84,14 +80,44 @@ window.findNQueensSolution = function(n) {
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   var solutionCount = 0; //fixme
-  if(n===0) {
-    solutionCount = 0;
-  } else if(n<3) {
+  if(n===0 || n===1) {
     solutionCount = 1;
-  } else if(n === 3) {
-    solutionCount = 2;
+  } else if(n<=3) {
+    solutionCount = 0;
   } else {
-    solutionCount = n;
+
+    // create new board of size n
+    var board = new Board({n:n});
+    // create solutionCount and set it 0
+    var solutionCount = 0;
+    // create function findValidPaths and receives board and current row index of board as parameter
+    var findValidPaths = function(board, rowIndex) {
+
+      // for each index of row
+      for(var i=0; i<n; i++) {
+        // if no conflicts at this index
+        board.togglePiece(rowIndex, i);
+        if(!board.hasColConflictAt(i) && !board.hasAnyMajorDiagonalConflicts() && !board.hasAnyMinorDiagonalConflicts()) {
+          // if row index = n
+          if(rowIndex === n-1) {
+            // increment solutionCount
+            solutionCount ++;
+          // else
+          } else {
+            // call recursively passing in updated board and current row index +1;
+            findValidPaths(board, rowIndex + 1);
+          }
+        }
+        // untoggle piece
+        board.togglePiece(rowIndex, i);
+        // console.log('untoggling', board.rows()[rowIndex][i], 'col index is ', i);
+      }
+    };
+    // findValidPaths(board, rowIndex)
+    findValidPaths(board, 0);
+    // return solutionCount
+    return solutionCount;
+
   }
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
